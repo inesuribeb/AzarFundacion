9e03e06345449179e1cde163b95efe56d456ca00
inesuribeb <inesuribeb@gmail.com>
2025-08-25 10:17:06 +0200

    pequeños cambios estilisticos de letra

--- mi-aplicacion/src/pages/Home/sections/Intro/Intro.jsx
+++ mi-aplicacion/src/pages/Home/sections/Intro/Intro.jsx
@@ -1,16 +1,16 @@
 import Texts from '../../../../components/Texts/Texts';
 import MediumSans from '../../../../components/Titles/MediumSans/MediumSans';
-import NavigationTabs from './components/NavigationTabs'; 
+import NavigationTabs2 from './components/NavigationTabs2'; 
 import './Intro.css'
 
 function Intro({ t, getRoute }) {
     return (
         <section className='intro-section'>
             {/* <Texts size="extra-large" className='intro-text-home'>{t('visionText')}</Texts> */}
-            <MediumSans className='intro-text-home'>{t('heroSubtitle')}</MediumSans>
+            <MediumSans className='intro-text-home2'>{t('heroSubtitle')}</MediumSans>
             <Texts className='intro-text-home'>{t('visionText')}</Texts>
             <div className='navigation-tabs'>
-                <NavigationTabs
+                <NavigationTabs2
                     t={t}
                     getRoute={getRoute}
                     onTabChange={(item) => console.log('Tab selected:', item)}
