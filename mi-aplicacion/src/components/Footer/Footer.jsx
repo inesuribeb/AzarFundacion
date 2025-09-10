766067058e326c0b7c6c19769bcf5ec30e93c665
inesuribeb <inesuribeb@gmail.com>
2025-09-10 18:08:33 +0200

    rediseño footer super minimal

--- mi-aplicacion/src/components/Footer/Footer.jsx
+++ mi-aplicacion/src/components/Footer/Footer.jsx
@@ -1,6 +1,10 @@
+import { Link } from 'react-router-dom';
+import { useLanguage } from '../../contexts/LanguageContext';
 import './Footer.css';
 
 function Footer() {
+    const { t, getRoute } = useLanguage();
+
     return (
         <div className='footer-content'>
             <div className='footer-anagram'>
@@ -8,27 +12,28 @@ function Footer() {
             </div>
             <div className='footer-info'>
                 <div className='footer-info-column'>
-                    <h3>Fundacion Azar</h3>
-                    <p>Fundación Azar</p>
-                    <p>Residencias</p>
-                    <p>Actividades</p>
-                    <p>Tienda</p>
+                    <a href="mailto:residencia@fundacionazar.com">
+                        residencia@fundacionazar.com
+                    </a>
+                </div>
+                <div className='footer-menu'>
+                    <Link to={getRoute('foundation')}>
+                        {t('foundation')}
+                    </Link>
+                    <Link to={getRoute('residenciesProgram')}>
+                        {t('residenciesProgram')}
+                    </Link>
+                    <Link to={getRoute('archive')}>
+                        {t('archive')}
+                    </Link>
+                    <Link to={`${getRoute('archive')}?tab=publications`}>
+                        {t('shop')}
+                    </Link>
                 </div>
                 <div className='footer-info-column'>
-                    <h3>Contacto</h3>
-                    <p>Email</p>
-                </div>
-                <div className='footer-info-column'>
-                    <h3>Síguenos</h3>
-                    <p>Instagram</p>
-                    <p>Tik Tok</p>
-                    <p>Linkedin</p>
-                </div>
-                <div className='footer-info-column'>
-                    <h3>Idiomas</h3>
-                    <p>Español</p>
-                    <p>Inglés</p>
-                    <p>Portugués</p>
+                    <a href="https://www.instagram.com/fundacionazar/" target="_blank" rel="noopener noreferrer">
+                        Instagram
+                    </a>
                 </div>
             </div>
         </div>
